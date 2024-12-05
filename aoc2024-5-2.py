828687e87182ef37129ac41bad8ccc4490e1ecfa
Ladislav ULLRICH <ladislav.ullrich@tipsport.cz>
2024-12-05 07:55:52 +0100

    Uloha 5 cast 2

---
+++ aoc2024-5-2.py
@@ -0,0 +1,53 @@
+def check_update(rules: dict, updatestr: str) -> bool:
+    update = updatestr.split(',')
+    if len(update) > 1:  # update musi byt delsi jak jedno cislo
+        for i in range(1, len(update)):
+            for j in range(0, i):
+                if update[i] in rules and update[j] in rules[update[i]]:
+                    return False
+    return True
+
+
+def fix_one_error(rules: dict, updatestr: str) -> str:
+    update = updatestr.split(',')
+    if len(update) > 1:  # update musi byt delsi jak jedno cislo
+        for i in range(1, len(update)):
+            for j in range(0, i):
+                if update[i] in rules and update[j] in rules[update[i]]:
+                    pom = update[i]
+                    update[i] = update[j]
+                    update[j] = pom
+                    return ','.join(update)
+    return updatestr
+
+
+def get_middle_number(updatestr: str) -> int:
+    numbers = (updatestr.split(','))
+    index = int((len(numbers) - 1) / 2)
+    return int(numbers[index])
+
+
+f = open("aoc2024-5-1-input.txt", "r")
+content = f.read()
+lines = content.splitlines()
+rules = {}
+idx = 0
+for line in lines:
+    idx += 1
+    if line.find('|') > 0:
+        rule = line.split('|')
+        if rule[0] in rules:
+            rules[rule[0]].add(rule[1])
+        else:
+            rules[rule[0]] = {rule[1]}
+    if line == '':
+        break
+
+result = 0
+for update in lines[-len(lines) + idx:]:
+    if check_update(rules, update):
+        continue
+    while not check_update(rules, update):
+        update = fix_one_error(rules, update)
+    result += get_middle_number(update)
+print(result)

415bf8bdb58a6d6a8b75daaa810a54235cfa4622
Ladislav ULLRICH <ladislav.ullrich@tipsport.cz>
2024-12-06 09:07:41 +0100

    6

---
+++ aoc2024-6-1.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+
+def prepare_grid_pos_agent(lines: list) -> tuple:
+    agent_pos = []
+    grid = []
+    for i in range(len(lines)):
+        grid.append([char for char in lines[i]])
+        for j in (range(len(lines[0]))):
+            if lines[i][j] in directions:
+                agent_pos = [i, j]
+    return agent_pos, grid
+
+
+def outside_grid(lines: list, next_y: int, next_x) -> bool:
+    if next_y == -1 or next_y == len(lines) or next_x == -1 or next_x == len(lines[0]):
+        return True
+    return False
+
+
+def pos_equals(pos1: list, pos2: list) -> bool:
+    return pos1[0] == pos2[0] and pos1[1] == pos2[1]
+
+
+f = open("aoc2024-6-1-input.txt", "r")
+content = f.read()
+lines = content.splitlines()
+
+directions = {'>': [0, 1], '^': [-1, 0], 'v': [1, 0], '<': [0, -1]}
+rotations = {'>': 'v', '^': '>', 'v': '<', '<': '^'}
+
+preparation = prepare_grid_pos_agent(lines)
+grid = preparation[1]
+first_direction = grid[preparation[0][0]][preparation[0][1]]
+solution_cnt = 0
+for y in range(len(grid)):
+    for x in range(len(grid[0])):
+        if grid[y][x] == '.' or grid[y][x] == 'X':
+            grid[y][x] = 'O'
+            actual_pos = preparation[0]
+            grid[actual_pos[0]][actual_pos[1]] = first_direction
+            start_pos = actual_pos
+            visited_cnt = 1
+            while not (visited_cnt > 1 and pos_equals(start_pos, actual_pos)):
+                print(np.matrix(grid))
+                print(actual_pos)
+                direction = grid[actual_pos[0]][actual_pos[1]]
+                next_y = actual_pos[0] + directions[direction][0]
+                next_x = actual_pos[1] + directions[direction][1]
+                if outside_grid(grid, next_y, next_x) or pos_equals(start_pos, [next_y, next_x]):
+                    break
+                next_object = grid[next_y][next_x]
+
+                if next_object == '.' or next_object == 'X':
+                    pom = grid[actual_pos[0]][actual_pos[1]]
+                    grid[actual_pos[0]][actual_pos[1]] = 'X'
+                    grid[next_y][next_x] = direction
+                    actual_pos = [next_y, next_x]
+                    if next_object == '.':
+                        visited_cnt += 1
+                elif next_object == '#' or next_object == 'O':
+                    grid[actual_pos[0]][actual_pos[1]] = rotations[direction]
+            if pos_equals(start_pos, actual_pos):
+                solution_cnt += 1
+                print('------------------')
+            if grid[y][x] == 'O':
+                grid[y][x] = '.'
+print(visited_cnt)
+print(solution_cnt)

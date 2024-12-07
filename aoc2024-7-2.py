bc4954fc43ce02b9f6d5b8aebbfc79ef2fcb7145
Ladislav ULLRICH <ladislav.ullrich@tipsport.cz>
2024-12-07 13:57:14 +0100

    7-2 review

--- aoc2024-7-2.py
+++ aoc2024-7-2.py
@@ -6,18 +6,31 @@ class TreeRoot:
         self.actual_value = operands[0]
         self.operation = str(self.actual_value) + ' '
         self.solution_found = False
+        self.leaf_with_solution: TreeNode = None
 
     def build_tree(self):
         TreeNode.root = self
-        child_operands = self.operands[1:]
-        self.childrens.append(TreeNode('*', self, self.actual_value, child_operands))
-        self.childrens.append(TreeNode('||', self, self.actual_value, child_operands))
-        self.childrens.append(TreeNode('+', self, self.actual_value, child_operands))
+        add_children(self)
         return self
+
+    def get_solution_of_equation(self) -> str:
+        if self.leaf_with_solution is not None:
+            node = self.leaf_with_solution
+            nodes: list[TreeNode] = []
+            while node != self:
+                nodes.append(node)
+                node = node.parent
+            solution = str(self.actual_value)
+            nodes.reverse()
+            for node in nodes:
+                solution += node.operator + str(node.operand)
+            return f"{self.result} = {solution}"
+        return 'Solution not found yet'
 
 
 class TreeNode:
     root: TreeRoot
+
     def __init__(self, operator, parent, parent_value, operands: list[int]):
         self.operator = operator
         self.parent = parent
@@ -38,24 +51,16 @@ class TreeNode:
             return
         if self.actual_value == result_of_eqation and len(self.operands) == 1:
             TreeNode.root.solution_found = True
-            #print(f"Solution found: {self.__get_equation_as_string()} == {result_of_eqation}")
+            TreeNode.root.leaf_with_solution = self
         elif len(self.operands) > 1 and self.actual_value <= result_of_eqation:
-            child_operands = self.operands[1:]
-            self.childrens.append(TreeNode('||', self, self.actual_value, child_operands))
-            self.childrens.append(TreeNode('*', self, self.actual_value, child_operands))
-            self.childrens.append(TreeNode('+', self, self.actual_value, child_operands))
+            add_children(self)
 
-    def __get_equation_as_string(self) -> str:
-        node = self
-        nodes: list[TreeNode] = []
-        while node != TreeNode.root:
-            nodes.append(node)
-            node = node.parent
-        solution = str(TreeNode.root.actual_value)
-        nodes.reverse()
-        for node in nodes:
-            solution += node.operator + str(node.operand)
-        return solution
+
+def add_children(node: TreeRoot | TreeNode):
+    child_operands = node.operands[1:]
+    node.childrens.append(TreeNode('*', node, node.actual_value, child_operands))
+    node.childrens.append(TreeNode('||', node, node.actual_value, child_operands))
+    node.childrens.append(TreeNode('+', node, node.actual_value, child_operands))
 
 
 f = open("aoc2024-7-1-input.txt", "r")
@@ -68,5 +73,6 @@ for line in lines:
     operands = [int(x) for x in data[1].strip().split(' ')]
     tree = TreeRoot(result_of_eqation, operands).build_tree()
     if tree.solution_found:
+        print(tree.get_solution_of_equation())
         solution += result_of_eqation
 print(solution)

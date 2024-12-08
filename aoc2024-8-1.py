99f58c1ce7e34e583ac57f34e994275beaa29805
Ladislav ULLRICH <ladislav.ullrich@tipsport.cz>
2024-12-08 14:21:10 +0100

    8-1 funkcni zasrana podminka funkce in_grid

--- aoc2024-8-1.py
+++ aoc2024-8-1.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def in_grid(lines: list[[any]], point: [int]) -> bool:
-    if point[0] == -1 or point[0] >= len(lines) or point[1] == -1 or point[1] >= len(lines[0]):
+    if (point[0] < 0 or point[0] >= len(lines)) or (point[1] < 0 or point[1] >= len(lines[0])):
         return False
     return True
 
@@ -17,26 +17,28 @@ for y in range(len(grid)):
     for x in range(len(grid)):
         char = grid[y][x]
         if char.isalnum():
+            # byla uz takova antena nalezena drive?
             if char in antennas:
-                # mam uz zalozeny seznam souradnic predchozich anten pro takovy znak?
-                for antenna in antennas[char]:  # pro vsechny stavajici anteny umistim antinode
-                    a1 = [y, x]
-                    a2 = [antenna[0], antenna[1]]
-                    # vector (a1 - a2) + A1 spocitam souradnice prvniho bodu pro umisteni antinode
-                    vect_a1_a2 = [a1[0] - a2[0], a1[1] - a2[1]]
-                    antinode_a1 = [a1[0] + vect_a1_a2[0], a1[1] + vect_a1_a2[1]]
-                    # vector (a2 - a1) + A2 souradnice druheho druheho bodu pro umisteni antinode
-                    vect_a2_a1 = [a2[0] - a1[0], a2[1] - a1[1]]
-                    antinode_a2 = [a2[0] + vect_a2_a1[0], a2[1] + vect_a2_a1[1]]
-                    if in_grid(grid, antinode_a1) and not antinode_at[antinode_a1[0]][antinode_a1[1]]:
-                        antinode_at[antinode_a1[0]][antinode_a1[1]] = True
+                for antenna in antennas[char]:
+                    r1, c1 = [y, x]  # antena 1
+                    r2, c2 = antenna  # antena 2
+
+                    # vypocet potencionalniho umisteni antinodu
+                    row_diff, col_diff = r2 - r1, c2 - c1
+                    # antinode, kde antena 1 je blize
+                    antinode1 = [r1 - row_diff, c1 - col_diff]
+                    # antinode, kde antena 2 je blize
+                    antinode2 = [r2 + row_diff, c2 + col_diff]
+
+                    if in_grid(grid, antinode1) and not antinode_at[antinode1[0]][antinode1[1]]:
+                        antinode_at[antinode1[0]][antinode1[1]] = True
                         antinodes_cnt += 1
-                    if in_grid(grid, antinode_a2) and not antinode_at[antinode_a2[0]][antinode_a2[1]]:
-                        antinode_at[antinode_a2[0]][antinode_a2[1]] = True
+                    if in_grid(grid, antinode2) and not antinode_at[antinode2[0]][antinode2[1]]:
+                        antinode_at[antinode2[0]][antinode2[1]] = True
                         antinodes_cnt += 1
-                # pridam novou, jiz vyrusenou s ostatnimi antenami do seznamu
+                # nova antena, pridam mezi ostatni, antinody spocitany
                 antennas[char].append([y, x])
-            else:  # tato antena je prvni s timto popiskem, zalozim pro ni novy seznam
+            else:  # nova antena prvni sveho druhu
                 antennas[char] = [[y, x]]
 
 print(antinodes_cnt)  # funguje na vzorovych prikladech a pro velky input vraci 389, coz pry neni spravna hodnota. Proc?!?!

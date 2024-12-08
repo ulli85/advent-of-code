2733aa49ae9b5d207911515d969f2cb748ced171
Ladislav ULLRICH <ladislav.ullrich@tipsport.cz>
2024-12-08 11:40:25 +0100

    8-1 promenne zmena pojmenovani

--- aoc2024-8-1.py
+++ aoc2024-8-1.py
@@ -9,18 +9,18 @@ def in_grid(lines: list[[any]], point: [int]) -> bool:
 
 f = open("aoc2024-8-1-input.txt", "r")
 content = f.read()
-lines = content.splitlines()
+grid = content.splitlines()
 solution = 0
 antennas = {}
 processed = {}
-antinode_at = np.full((len(lines), len(lines)), False, dtype=bool)
+antinode_at = np.full((len(grid), len(grid)), False, dtype=bool)
 antinodes_cnt = 0
-for y in range(len(lines)):
-    for x in range(len(lines)):
-        if lines[y][x].isalnum():
-            if lines[y][x] in antennas:
+for y in range(len(grid)):
+    for x in range(len(grid)):
+        if grid[y][x].isalnum():
+            if grid[y][x] in antennas:
                 # pokud uz takto pojmenovana antena je, musim ji vyrusit se vsemi jiz znamymi
-                for antenna in antennas[lines[y][x]]:
+                for antenna in antennas[grid[y][x]]:
                     a1 = [y, x]
                     a2 = [antenna[0], antenna[1]]
                     # vector (a1 - a2) + A1 souradnice prvniho bodu pro blokovani
@@ -29,30 +29,30 @@ for y in range(len(lines)):
                     # vector (a2 - a1) + A2 souradnice druheho bodu pro blokovani
                     vect_a2_a1 = [a2[0] - a1[0], a2[1] - a1[1]]
                     antinode_a2 = [a2[0] + vect_a2_a1[0], a2[1] + vect_a2_a1[1]]
-                    if in_grid(lines, antinode_a1) and not antinode_at[antinode_a1[0]][antinode_a1[1]]:
+                    if in_grid(grid, antinode_a1) and not antinode_at[antinode_a1[0]][antinode_a1[1]]:
                         antinode_at[antinode_a1[0]][antinode_a1[1]] = True
                         antinodes_cnt += 1
-                    if in_grid(lines, antinode_a2) and not antinode_at[antinode_a2[0]][antinode_a2[1]]:
+                    if in_grid(grid, antinode_a2) and not antinode_at[antinode_a2[0]][antinode_a2[1]]:
                         antinode_at[antinode_a2[0]][antinode_a2[1]] = True
                         antinodes_cnt += 1
                 # pridam novou, jiz vyrusenou s ostatnimi antenami do seznamu
-                antennas[lines[y][x]].append([y, x])
+                antennas[grid[y][x]].append([y, x])
             else:  # antena je prvni s danym oznacenim, zalozim pro ni novy seznam
-                antennas[lines[y][x]] = [[y, x]]
+                antennas[grid[y][x]] = [[y, x]]
 debug = True
 if debug:  ## nemam poneti co s tim, chybu nevidim, tak si to alespon hezky vytisknu
-    view = np.full((len(lines), len(lines)), '.', dtype=str)
+    solution_view = np.full((len(grid), len(grid)), '.', dtype=str)
     for key in antennas.keys():
         for antena in antennas[key]:
-            view[antena[0]][antena[1]] = key
-    for y in range(len(lines)):
-        for x in range(len(lines)):
+            solution_view[antena[0]][antena[1]] = key
+    for y in range(len(grid)):
+        for x in range(len(grid)):
             if antinode_at[y][x]:
-                view[y][x] = '~' if str(view[y][x]).isalnum() else '#'
-    view_compact = []
-    for y in range(len(lines)):
-        view_compact.append(lines[y] + '    ' + ''.join(view[y]))
+                solution_view[y][x] = '~' if str(solution_view[y][x]).isalnum() else '#'
+    view_entry_and_solution_side_by_side = []
+    for y in range(len(grid)):
+        view_entry_and_solution_side_by_side.append(grid[y] + '    ' + ''.join(solution_view[y]))
     print('\n')
-    print('\n'.join(view_compact))
+    print('\n'.join(view_entry_and_solution_side_by_side))
     print(antennas)
 print(antinodes_cnt)  # funguje na vzorovych prikladech ale ne pro velky input :-(

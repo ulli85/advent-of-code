fdaf8a7bc3264dbf0369aa10f0efa1420965796a
Ladislav ULLRICH <ladislav.ullrich@tipsport.cz>
2024-12-09 10:15:22 +0100

    9-1 still not working debuglog
    memory: 2333133121414131402
    ------init------
    files: [(0, 2), (1, 3), (2, 1), (3, 3), (4, 2), (5, 4), (6, 4), (7, 3), (8, 4), (9, 2)]
    space: [3, 3, 3, 1, 1, 1, 1, 1, 0]
    spmem:
    
    ------file_remove-----
    files: [(1, 3), (2, 1), (3, 3), (4, 2), (5, 4), (6, 4), (7, 3), (8, 4), (9, 2)]
    space: [3, 3, 3, 1, 1, 1, 1, 1, 0]
    spmem: 00
    
    ------free_space------
    files: [(1, 3), (2, 1), (3, 3), (4, 2), (5, 4), (6, 4), (7, 3), (8, 4)]
    space: [1, 3, 3, 1, 1, 1, 1, 1, 0]
    spmem: 0099
    
    ------free_space------
    files: [(1, 3), (2, 1), (3, 3), (4, 2), (5, 4), (6, 4), (7, 3), (8, 3)]
    space: [3, 3, 1, 1, 1, 1, 1, 0]
    spmem: 00998
    
    ------file_remove-----
    files: [(2, 1), (3, 3), (4, 2), (5, 4), (6, 4), (7, 3), (8, 3)]
    space: [3, 3, 1, 1, 1, 1, 1, 0]
    spmem: 00998111
    
    ------free_space------
    files: [(2, 1), (3, 3), (4, 2), (5, 4), (6, 4), (7, 3)]
    space: [3, 1, 1, 1, 1, 1, 0]
    spmem: 00998111888
    
    ------file_remove-----
    files: [(3, 3), (4, 2), (5, 4), (6, 4), (7, 3)]
    space: [3, 1, 1, 1, 1, 1, 0]
    spmem: 009981118882
    
    ------free_space------
    files: [(3, 3), (4, 2), (5, 4), (6, 4)]
    space: [1, 1, 1, 1, 1, 0]
    spmem: 009981118882777
    
    ------file_remove-----
    files: [(4, 2), (5, 4), (6, 4)]
    space: [1, 1, 1, 1, 1, 0]
    spmem: 009981118882777333
    
    ------free_space------
    files: [(4, 2), (5, 4), (6, 3)]
    space: [1, 1, 1, 1, 0]
    spmem: 0099811188827773336
    
    ------file_remove-----
    files: [(5, 4), (6, 3)]
    space: [1, 1, 1, 1, 0]
    spmem: 009981118882777333644
    
    ------free_space------
    files: [(5, 4), (6, 2)]
    space: [1, 1, 1, 0]
    spmem: 0099811188827773336446
    
    ------file_remove-----
    files: [(6, 2)]
    space: [1, 1, 1, 0]
    spmem: 00998111888277733364465555
    
    ------free_space------
    files: [(6, 1)]
    space: [1, 1, 0]
    spmem: 009981118882777333644655556
    
    ------file_remove-----
    files: []
    space: [1, 1, 0]
    spmem: 0099811188827773336446555566
    
    solution = 1928

--- aoc2024-9-1.py
+++ aoc2024-9-1.py
@@ -1,20 +1,36 @@
+def print_stats(debug:bool, is_free_space: bool=False, optional:str=None):
+    if debug:
+        if optional is not None:
+            print(f"------{optional}------")
+        else:
+            if is_free_space:
+                print(f"------free_space------")
+            else:
+                print(f"------file_remove-----")
+        print(f"files: {files}")
+        print(f"space: {free_space}")
+        print(f"spmem: {sparse_mem}\n")
+
 f = open("aoc2024-9-1-input.txt", "r")
 memory = f.read()
-
-# kazdy sudy cislo je volne misto
+print(f"memory: {memory}")
 free_space = [int(x) for x in memory[1::2]]
+#list of files, each as (file_id, file_blocks)
 files = list(enumerate([int(x) for x in memory[0::2]]))
-print(f"files: {files}")
-print(f"space: {free_space}")
-
 sparse_mem = ''
+debug = True
+print_stats(debug,False,'init')
+
 while len(files) > 0:
     file = files[0]
     sparse_mem += str(file[0]) * int(file[1])
     files = files[1::]
-    if len(free_space) > 0:  # do we have free space ?
+    print_stats(debug)
+
+    if len(free_space) > 0 and len(files) > 0:  # do we have free space and files to compact ?
         free_blocks = int(free_space[0])
-        while free_blocks > 0 and len(files) > 0:
+        # do until I have free_blocks to use
+        while free_blocks > 0:
             last_file = files[-1]
             file_id = last_file[0]
             file_blocks = last_file[1]
@@ -27,17 +43,13 @@ while len(files) > 0:
                 free_space = free_space[1::]  # discard free space
             else:
                 free_space[0] = free_blocks  # update free space block count
-
             if file_blocks == 0:
                 files = files[0:len(files) - 1:]  # discard last file
             else:
                 last_file = (file_id, file_blocks)  # update file block count
                 files[-1] = last_file
-print(sparse_mem)
+            print_stats(debug, True)
 solution = 0
 for i in range(len(sparse_mem)):
     solution += i * int(sparse_mem[i])
 print(f"solution = {solution}")
-print(memory)
-print(f"files: {files}")
-print(f"space: {free_space}")

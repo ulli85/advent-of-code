6b591d0903e0a1ea8e9a7b6d0a9d5e5de897b02b
Ladislav ULLRICH <ladislav.ullrich@tipsport.cz>
2024-12-11 10:22:57 +0100

    11-1

---
+++ aoc2024-11-1.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+def blink(num: int):
+    if num == 0: return [1]
+    if len(str(num)) % 2 == 0:
+        half = int(len(str(num)) / 2)
+        left = str(num)[0:half]
+        right = str(num)[half::]
+        return [int(left), int(right)]
+    return [2024 * num]
+
+
+f = open("aoc2024-11-1-input.txt", "r")
+input = [int(x) for x in f.read().split()]
+stones = deque(input)
+blink_cnt = 25
+for i in range(blink_cnt):
+    stones_nxt = deque()
+    for stone in stones:
+        st_nxt = blink(stone)
+        for nxt in st_nxt:
+            stones_nxt.append(nxt)
+    stones = stones_nxt
+print(len(stones))

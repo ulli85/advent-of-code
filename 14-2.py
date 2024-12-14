dca736fb8b1edcff795cc8dd5c22d86c3e8a2540
Ladislav ULLRICH <ladislav.ullrich@tipsport.cz>
2024-12-15 00:43:40 +0100

    14-2

--- 14-2.py
+++ 14-2.py
@@ -9,7 +9,7 @@ Y_HALF = (HEIGHT - 1) // 2
 
 res = re.findall("p=(\\d+),(\\d+) v=(-?\\d+),(-?\\d+)", open("input/14-1.txt").read())
 
-min_mp = 999999999999999
+longest_sequence = 0
 best_step = 0
 for step in range(1, WIDTH * HEIGHT + 1):
     lu = ld = ru = rd = 0
@@ -18,20 +18,19 @@ for step in range(1, WIDTH * HEIGHT + 1):
         px, py, vx, vy = int(robot[0]), int(robot[1]), int(robot[2]), int(robot[3])
         xn, yn = (px + vx * step) % WIDTH, (py + vy * step) % HEIGHT
         visual[yn][xn] = '*'
-        if yn < Y_HALF:
-            if xn < X_HALF:
-                lu += 1
-            if xn > X_HALF:
-                ru += 1
-        if yn > Y_HALF:
-            if xn < X_HALF:
-                ld += 1
-            if xn > X_HALF:
-                rd += 1
-
-    sm = lu * ld * ru * rd
-    if sm < min_mp:
-        min_mp = sm
-        best_step = step
+    x = X_HALF + 1
+    y = 0
+    while y < HEIGHT:
+        while y < HEIGHT and visual[y][x] == '.':
+            y += 1
+        sequence = 0
+        while y < HEIGHT and visual[y][x] == '*':
+            sequence += 1
+            y += 1
+        if sequence > longest_sequence:
+            longest_sequence = sequence
+            best_step = step
+            for line in visual:
+                print(''.join(line))
 print(best_step)
 

16c8824a29617d363f70c311a70ca9cb2256b44e
LADISLAV ULLRICH <ladislav.ullrich@tipsport.cz>
2024-12-22 11:08:11 +0100

    22-2 minor change

--- 22-2.py
+++ 22-2.py
@@ -13,16 +13,13 @@ def evolve(sn: int) -> int:
 sequence_map = {}
 sequence_arr = deque()
 max_bananas_cnt = 0
-sum_of_secrets = 0
 for snid, sn in enumerate(input):
     last_digit_before = sn % 10
     unique = set()
-    k = 0
     for i in range(0, 2000):
         sn = evolve(sn)
         if sn not in unique:
             unique.add(sn)
-            # sum_of_secrets += secret_n
             last_digit = sn % 10
             bananas_change = last_digit - last_digit_before
             last_digit_before = last_digit
@@ -43,7 +40,4 @@ for snid, sn in enumerate(input):
                     if max_bananas_cnt < last_digit:
                         max_bananas_cnt = last_digit
 
-# for i in sequence_map.items():
-#    print(f'{i[1]}x {i[0]}')
-# print(sum_of_secrets)
 print(max_bananas_cnt)

72dfe72c35a74375b82a0ade2798c86dc317acd1
LADISLAV ULLRICH <ladislav.ullrich@tipsport.cz>
2024-12-25 11:11:22 +0100

    25-1

---
+++ 25-1.py
@@ -0,0 +1,35 @@
+lines = [str(x) for x in open("input/25-1.txt").read().splitlines()]
+print(lines)
+KEYS = []
+LOCKS = []
+ROW_SZ = 5
+COL_SZ = 7
+
+obj = []
+for i, line in enumerate(lines):
+    if line != '': obj.append(line)
+    if line == '' or i == len(lines) - 1:
+        digits = []
+        for i in range(ROW_SZ):
+            cdgit = -1
+            for j in range(COL_SZ):
+                if obj[j][i] == '#': cdgit += 1
+            digits.append(cdgit)
+        if obj[0][0] == '#':
+            LOCKS.append(digits)
+        else:
+            KEYS.append(digits)
+        obj.clear()
+
+# compare keys and locks - they fit if any of all column does not overlap
+pairs = 0
+for lock in LOCKS:
+    for key in KEYS:
+        fit = True
+        for i in range(ROW_SZ):
+            if lock[i] + key[i] >= COL_SZ - 1:
+                fit = False
+                break
+        if fit: pairs += 1
+        print(f'Lock {",".join([str(x) for x in lock])} and key {",".join([str(x) for x in key])} {"fit" if fit else "overlap"}')
+print(pairs)

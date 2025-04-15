cb7731fb3e9917d4694561531fc380be342001ab
LADISLAV ULLRICH <ladislav.ullrich@tipsport.cz>
2025-04-15 15:08:52 +0200

    AOC 2023 2-2

---
+++ 2023/2-2.py
@@ -0,0 +1,7 @@
+import re
+from functools import reduce
+
+print(sum([reduce(lambda x,y: x * y, row) for row in list(map(lambda r: [
+     max([int(x) for x in re.findall('(\\d+) red', r)]),
+     max([int(x) for x in re.findall('(\\d+) blue', r)]),
+     max([int(x) for x in re.findall('(\\d+) green', r)])], open("input/2").read().splitlines()))]))

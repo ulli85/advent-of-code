1c07a96bd71222257b25d1d7e77baf6f899d90df
LADISLAV ULLRICH <ladislav.ullrich@tipsport.cz>
2025-04-18 00:12:41 +0200

    AOC 2023 4-1

---
+++ 2023/4-1.py
@@ -0,0 +1,9 @@
+import math
+import re
+
+data = list(map(lambda x: re.fullmatch('^Card\\s+\\d+:\\s+(?P<winning_numbers>.*)\\|\\s+(?P<mine_numbers>.*)$', x), open("input/4").read().splitlines()))
+suma = 0
+for game in data:
+    matched_numbers = {*game.group('winning_numbers').split()}.intersection({*game.group('mine_numbers').split()})
+    suma += int(math.pow(2, len(matched_numbers) - 1))
+print(suma)

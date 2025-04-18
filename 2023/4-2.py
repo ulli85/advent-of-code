74d6a0265bd103456dae7ab7f54bd8221d2c75f0
LADISLAV ULLRICH <ladislav.ullrich@tipsport.cz>
2025-04-18 11:28:06 +0200

    AOC 2023 4-2

--- 2023/4-2.py
+++ 2023/4-2.py
@@ -1,10 +1,13 @@
-import math
 import re
 
 data = open("input/4").read().splitlines()
+cards = dict(zip([i for i in range(1, len(data) + 1)], len(data) * [1]))
 matches = map(lambda x: re.fullmatch('^Card\\s+\\d+:\\s+(?P<winning_nums>.*)\\|\\s+(?P<mine_nums>.*)$', x), data)
-suma = 0
-for match in matches:
-    matched_numbers = {*match.group('winning_nums').split()}.intersection({*match.group('mine_nums').split()})
-    suma += int(math.pow(2, len(matched_numbers) - 1))
-print(suma)
+for game_id, match in enumerate(matches, 1):
+    matchings = {*match.group('winning_nums').split()}.intersection({*match.group('mine_nums').split()})
+    card_copies = cards[game_id]
+    for i in range(game_id + 1 , game_id + len(matchings) + 1):
+        if i not in cards: break
+        cards[i] += card_copies
+
+print(sum(cards.values()))

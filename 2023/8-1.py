b385cd097eb3cdc48cfc0be96ea86aae708aa60c
LADISLAV ULLRICH <ladislav.ullrich@tipsport.cz>
2025-05-29 19:45:22 +0200

    AOC 2023 8-1

--- 2023/8-1.py
+++ 2023/8-1.py
@@ -6,14 +6,11 @@ network = {k: v for k, v in
            list(map(lambda a: (a[0], (a[1], a[2])), map(lambda line: re.findall('\\w{3}', line), lines[2:])))}
 
 dir_2_index = lambda x: 0 if x == 'L' else 1
-now, dest = 'AAA', 'ZZZ'
-
-for step in range(10000000):
+now = 'AAA'
+step = 0
+while now != 'ZZZ':
     direction = instructions[step % len(instructions)]
     next_nodes = network[now]
     now = next_nodes[dir_2_index(direction)]
-
-    if now == dest:
-        print(step + 1)
-        exit(0)
-print(f'Path not found actual: {now}')
+    step += 1
+print(step)

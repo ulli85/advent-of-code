6b051f99ec969bab07b6dc95eed3c24a6ecdfcf5
LADISLAV ULLRICH <ladislav.ullrich@tipsport.cz>
2025-05-29 19:52:27 +0200

    AOC 2023 8-1, 8-2 little changes

--- 2023/8-1.py
+++ 2023/8-1.py
@@ -3,14 +3,13 @@ import re
 lines = open('input/8').read().splitlines()
 instructions = lines[0]
 network = {k: v for k, v in
-           list(map(lambda a: (a[0], (a[1], a[2])), map(lambda line: re.findall('\\w{3}', line), lines[2:])))}
+           list(map(lambda a: (a[0], {'L': a[1], 'R': a[2]}), map(lambda line: re.findall('\\w{3}', line), lines[2:])))}
 
-dir_2_index = lambda x: 0 if x == 'L' else 1
-now = 'AAA'
-step = 0
+now, step = 'AAA', 0
+
 while now != 'ZZZ':
     direction = instructions[step % len(instructions)]
     next_nodes = network[now]
-    now = next_nodes[dir_2_index(direction)]
+    now = next_nodes[direction]
     step += 1
 print(step)

7c968f7e5981d1f2f9fd958906cd42cd5bab5ecc
LADISLAV ULLRICH <ladislav.ullrich@tipsport.cz>
2025-12-04 06:46:05 +0100

    AOC 2025 4-1, 4-2

--- 2025/4-1.py
+++ 2025/4-1.py
@@ -4,11 +4,11 @@ all_rolls = 0
 for y in range(0, len(maze)):
     for x in range(0, len(maze)):
         if maze[y][x] != '@': continue
-        adj_pap = 0
+        adj_rolls = 0
         for ay, ax in [(y - 1, x), (y - 1, x - 1), (y - 1, x + 1), (y, x - 1),
                        (y, x + 1), (y + 1, x), (y + 1, x - 1), (y + 1, x + 1)]:
             if 0 <= ay < len(maze) and 0 <= ax < len(maze):
-                adj_pap += 1 if maze[ay][ax] == '@' else 0
-        if adj_pap < 4:
+                adj_rolls += 1 if maze[ay][ax] == '@' else 0
+        if adj_rolls < 4:
             all_rolls += 1
 print(all_rolls)

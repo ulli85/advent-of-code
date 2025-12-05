3a0ebf3247db8b666b7b9a143b1d386c50413e48
LADISLAV ULLRICH <ladislav.ullrich@tipsport.cz>
2025-12-05 13:30:52 +0100

    AOC 2025 5-2

--- 2025/5-2.py
+++ 2025/5-2.py
@@ -1,15 +1,10 @@
 data = open('input/5.txt').read().splitlines()
 rng = sorted(list(map(lambda x: list(map(int, x.split('-'))), data[0: data.index('')])), key=lambda x: x[0])
-
-interchange = True
-while interchange:
-    interchange = False
-    for i in range(0, len(rng) - 1):
-        if rng[i][1] >= rng[i + 1][0]:
-            new_range = [rng[i][0], max(rng[i][1], rng[i + 1][1])]
-            rng = rng[0:i] + [new_range] + rng[i + 2:]
-            interchange = True
-            break
-
-result = sum(map(lambda x: x[1] - x[0] + 1, rng))
-print(result)
+res = []
+i = 0
+while i < len(rng) - 1:
+    if rng[i][1] >= rng[i + 1][0]:
+        new_range = [rng[i][0], max(rng[i][1], rng[i + 1][1])]
+        rng = rng[0:i] + [new_range] + rng[i + 2:]
+    else: i += 1
+print(sum(map(lambda x: x[1] - x[0] + 1, rng)))

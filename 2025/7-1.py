3cfd3a40971b5c547f284cb4769646f7cc4a0df1
LADISLAV ULLRICH <ladislav.ullrich@tipsport.cz>
2025-12-07 13:33:52 +0100

    AOC 2025 7-1

--- 2025/7-1.py
+++ 2025/7-1.py
@@ -9,6 +9,7 @@ beams[s] = 1
 
 total = 0
 for i, r in enumerate(grid[1:], 1):
+    beams_n = [0] * width
     for k, b in enumerate(beams):
         if beams[k] == 0: continue
         if r[k] == '^':
@@ -17,6 +18,5 @@ for i, r in enumerate(grid[1:], 1):
         else:
             beams_n[k] = 1
     total += len(list(filter(lambda x: x == (1, 0), zip(beams, beams_n))))
-    beams = beams_n.copy()
-    beams_n = [0] * width
+    beams = beams_n
 print(total)

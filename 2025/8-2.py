ebd6c101c7211b3117ec088a93481d75a62c6e3b
LADISLAV ULLRICH <ladislav.ullrich@tipsport.cz>
2025-12-09 21:40:12 +0100

    AOC 2025 8-2

---
+++ 2025/8-2.py
@@ -0,0 +1,43 @@
+import math
+from functools import reduce
+
+
+def distance(c1, c2):
+    return round(math.sqrt(pow(c1[0] - c2[0], 2) + pow(c1[1] - c2[1], 2) + pow(c1[2] - c2[2], 2)), 3)
+
+
+data = list(map(lambda x: tuple(map(int, x.split(','))), open('input/8.txt').read().splitlines()))
+DISTANCES = {}
+
+for k in data:
+    for j in data:
+        if k != j and (k, j) not in DISTANCES and (j, k) not in DISTANCES:
+            DISTANCES[(k, j)] = distance(k, j)
+
+DISTANCES_SORTED = {k: v for k, v in sorted(DISTANCES.items(), key=lambda item: item[1])}
+COORD_2_GRP_ID = dict(zip(data, [i for i in range(0, len(data))]))
+GRP_ID_2_COORDS = dict(zip([i for i in range(0, len(data))], [set() for i in range(0, len(data))]))
+actual_connection = 0
+dst_iter = iter(DISTANCES_SORTED)
+
+res = 0
+while len(GRP_ID_2_COORDS) > 1:
+    actual_connection += 1
+    c1, c2 = next(dst_iter)
+    print(f'#{actual_connection} {c1} -> {c2} Dst: {DISTANCES_SORTED[(c1, c2)]}')
+    gr_id1, gr_id2 = COORD_2_GRP_ID[c1], COORD_2_GRP_ID[c2]
+    if gr_id1 == gr_id2:
+        # print('Same group, skip. Connection counts')
+        continue
+    res = c1[0] *  c2[0]
+    COORD_2_GRP_ID[c2] = gr_id1
+    group1, group2 = GRP_ID_2_COORDS[gr_id1], GRP_ID_2_COORDS[gr_id2]
+    group1 = group1.union([c2])
+    group2 = group2.union([c1])
+    for c_in_g2 in group2:
+        COORD_2_GRP_ID[c_in_g2] = gr_id1
+    united_groups = group1.union(group2)
+    GRP_ID_2_COORDS[gr_id1] = united_groups
+    GRP_ID_2_COORDS.pop(gr_id2)
+    # print(list(sorted(filter(lambda g: g > 0, map(len, GRP_ID_2_COORDS.values())), reverse=True)))
+print(res)
